c4a819ffeb62c4144409b947cd345760fdd545db
lakshayk12 <lakshayk12@yahoo.com>
2019-02-09 14:56:58 +0530

    working on Multi class

--- Main.py
+++ Main.py
@@ -22,6 +22,12 @@ def get_dataset_ready(filename):
     X = data[:, 0:len(data[0]) - 1]
     Y = data[:, len(data[0]) - 1]
     Y = np.reshape(Y, newshape=(len(Y), 1))
+    # one hot labeling classes needs to be stated as 0 1 2 . . .
+    unique_classes = np.unique(Y)
+    one_hot_labels = np.zeros((Y.shape[0], len(unique_classes)))
+    for i in range(one_hot_labels.shape[0]):
+        one_hot_labels[i, int(Y[i, 0])] = 1
+    Y = one_hot_labels
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
     return x_train, x_test, y_train, y_test
 
@@ -35,8 +41,9 @@ def scale(x_train, x_test):
 
 if __name__ == '__main__':
     start_time = time.time()
+    # print(np.log(0))
     x_train = [[0, 0], [0, 1], [1, 0], [1, 1]]
-    y_train = [[0], [1], [1], [1]]
+    y_train = [[1, 0], [0, 1], [0, 1], [0, 1]]
     x_train = np.array(x_train)
     y_train = np.array(y_train)
     x_test = x_train
@@ -44,6 +51,6 @@ if __name__ == '__main__':
     # x_train, x_test, y_train, y_test = get_dataset_ready('Parkinsons.csv')
     # x_train, x_test = scale(x_train, x_test)
     # inbuilt_algo(x_train, x_test, y_train, y_test)
-    PSO.model(x_train, x_test, y_train, y_test, len(x_train[0]), 2, 2, 2)
+    unique_classes = 2  # that equals no. of output neurons
+    PSO.model(x_train, x_test, y_train, y_test, len(x_train[0]), 2, 2, unique_classes)
     print("Execution Time:", time.time() - start_time)
-    exit()
